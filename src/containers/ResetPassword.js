42af50d62348474d02ce48291d3e8f1af6e2bafe
Joshua Minzner <josh@minzner.org>
2016-05-06 12:54:19 -0400

    Add styles to reset password page.

--- src/containers/ResetPassword.js
+++ src/containers/ResetPassword.js
@@ -3,6 +3,19 @@ import { reduxForm } from 'redux-form';
 import { resetPassword } from '../actions/auth';
 import { compose } from 'redux';
 import { pageify } from '../utils/page';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import classnames from 'classnames';
+
+function tooltip(
+    text,
+    id,
+    trigger = <i className="fa fa-question-circle" aria-hidden="true" />,
+    placement = 'right'
+) {
+    const tip = <Tooltip id={`tooltip--${id}`}>{text}</Tooltip>;
+
+    return <OverlayTrigger placement={placement} overlay={tip}>{trigger}</OverlayTrigger>;
+}
 
 export class ResetPassword extends Component {
     constructor() {
@@ -25,25 +38,60 @@ export class ResetPassword extends Component {
     render() {
         const {
             fields: { newPassword, newPasswordRepeat },
-            handleSubmit, submitFailed, valid, error,
+            handleSubmit, submitFailed, valid, error, submitting, pristine,
             page: { submitSuccess }
         } = this.props;
 
-        return <form onSubmit={handleSubmit(this.send)}>
-            <div><label>New Password:</label><input type="password" {...newPassword}/></div>
-            <div>
-                <label>New Password (Again):</label>
-                <input type="password" {...newPasswordRepeat}/>
+        return (<div className="container main-section">
+            <div className="row">
+                <div className="login-form col-md-4 col-md-offset-4 col-xs-12
+                    animated fadeIn card-item">
+                    <h1 className="text-center">Reset Password</h1>
+                    <form onSubmit={handleSubmit(this.send)}>
+                        <div className="form-group">
+                            <label htmlFor="inputHelpBlock">
+                                New Password {tooltip('Your new password.', 'new-password')}
+                            </label>
+                            <input type="password" {...newPassword} className="form-control" />
+                        </div>
+                        <div className={classnames('form-group', {
+                            'has-feedback': newPasswordRepeat.value && newPassword.error,
+                            'has-warning': newPasswordRepeat.value && newPassword.error
+                        })}>
+                            <label htmlFor="inputHelpBlock">
+                                Confirm New Password 
+                                {tooltip('Your new password (again.)', 'new-password-again')}
+                            </label>
+                            <input type="password" {...newPasswordRepeat} className="form-control"/>
+                            {newPasswordRepeat.value && newPassword.error && tooltip(
+                                newPassword.error,
+                                'password-mismatch',
+                                <span className="glyphicon glyphicon-warning-sign
+                                    form-control-feedback"
+                                    aria-hidden="true">
+                                </span>
+                            )}
+                        </div>
+                        {submitSuccess && (<div className="alert alert-success" role="alert">
+                            <strong>Your password has been reset!</strong>
+                        </div>)}
+                        {error && submitFailed && !submitting && (
+                            <div className="alert alert-danger" role="alert">
+                                <strong>Uh-oh!</strong> {error.response}
+                            </div>
+                        )}
+                        <button type="submit"
+                            disabled={submitting || pristine || !valid}
+                            className={classnames('btn btn-danger btn-lg btn-block', {
+                                'btn-waiting': submitting
+                            })}>
+                            Confirm Reset
+                        </button>
+                    </form>
+                    <br />
+                </div>
             </div>
-            {newPassword.touched && newPasswordRepeat.touched && newPassword.error && (
-                <div>{newPassword.error}</div>
-            )}
-
-            {submitFailed && error && (<div>{error.response}</div>)}
-            {submitSuccess && (<div>Successfully changed password!</div>)}
-
-            <div><button disabled={!valid} type="submit">Change Password</button></div>
-        </form>;
+        </div>);
     }
 }
 
@@ -51,11 +99,13 @@ ResetPassword.propTypes = {
     fields: PropTypes.object.isRequired,
     handleSubmit: PropTypes.func.isRequired,
     submitting: PropTypes.bool.isRequired,
-    error: PropTypes.any,
+    error: PropTypes.instanceOf(Error),
+    submitFailed: PropTypes.bool.isRequired,
+    valid: PropTypes.bool.isRequired,
+    pristine: PropTypes.bool.isRequired,
+
     page: PropTypes.object.isRequired,
-    location: PropTypes.object.isRequired,
-    submitFailed: PropTypes.bool,
-    valid: PropTypes.bool
+    location: PropTypes.object.isRequired
 };
 
 export function validate(values) {
