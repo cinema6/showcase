6840f97c869b2f314570d55db735d81d6c27c636
Joshua Minzner <josh@minzner.org>
2016-05-16 08:35:06 -0400

    Don't include logging middleware in production.

--- test/spec/configure_store.ut.js
+++ test/spec/configure_store.ut.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/* global process */
+
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
 import promisify from '../../src/middleware/promisify';
@@ -12,33 +14,13 @@ describe('configureStore(initialState)', function() {
     let configureStore;
     let redux, reactRouterRedux, reduxLogger, reduxPromiseMiddleware;
 
-    beforeEach(function() {
-        redux  = {
-            createStore: jasmine.createSpy('createStore()').and.callFake(require('redux').createStore),
-            applyMiddleware: jasmine.createSpy('applyMiddleware()').and.callFake(require('redux').applyMiddleware),
-
-            __esModule: true
-        };
-
-        reactRouterRedux = {
-            routerMiddleware: jasmine.createSpy('routerMiddleware()').and.callFake(require('react-router-redux').routerMiddleware),
-
-            __esModule: true
-        };
-
-        reduxPromiseMiddleware = {
-            default: jasmine.createSpy('promiseMiddleware()').and.callFake(require('redux-promise-middleware').default),
-
-            __esModule: true
-        };
-
-        reduxLogger = jasmine.createSpy('reduxLogger()').and.callFake(require('redux-logger'));
-
-        configureStore = proxyquire('../../src/store/configure_store', {
+    function get() {
+        return proxyquire('../../src/store/configure_store', {
             redux,
             'react-router-redux': reactRouterRedux,
             'redux-logger': reduxLogger,
             'redux-promise-middleware': reduxPromiseMiddleware,
+            '_process': process,
 
             '../reducers': {
                 default: require('../../src/reducers').default,
@@ -70,6 +52,31 @@ describe('configureStore(initialState)', function() {
                 __esModule: true
             }
         }).default;
+    }
+
+    beforeEach(function() {
+        redux  = {
+            createStore: jasmine.createSpy('createStore()').and.callFake(require('redux').createStore),
+            applyMiddleware: jasmine.createSpy('applyMiddleware()').and.callFake(require('redux').applyMiddleware),
+
+            __esModule: true
+        };
+
+        reactRouterRedux = {
+            routerMiddleware: jasmine.createSpy('routerMiddleware()').and.callFake(require('react-router-redux').routerMiddleware),
+
+            __esModule: true
+        };
+
+        reduxPromiseMiddleware = {
+            default: jasmine.createSpy('promiseMiddleware()').and.callFake(require('redux-promise-middleware').default),
+
+            __esModule: true
+        };
+
+        reduxLogger = jasmine.createSpy('reduxLogger()').and.callFake(require('redux-logger'));
+
+        configureStore = get();
     });
 
     it('should exist', function() {
@@ -118,6 +125,27 @@ describe('configureStore(initialState)', function() {
 
         it('should return the store', function() {
             expect(result).toBe(redux.createStore.calls.mostRecent().returnValue);
+        });
+
+        describe('if in production', function() {
+            beforeEach(function() {
+                process.env.NODE_ENV = 'production';
+                reduxLogger.calls.reset();
+                redux.createStore.calls.reset();
+                configureStore = get();
+
+                result = configureStore(initialState);
+            });
+
+            it('should not include the logger middleware', function() {
+                expect(reduxLogger).not.toHaveBeenCalled();
+                expect(redux.applyMiddleware.calls.mostRecent().args.length).toBe(redux.applyMiddleware.calls.first().args.length - 1, 'Did not apply fewer middlewares.');
+            });
+
+            it('should create a return a store', function() {
+                expect(redux.createStore).toHaveBeenCalledWith(rootReducer, initialState, redux.applyMiddleware.calls.mostRecent().returnValue);
+                expect(result).toBe(redux.createStore.calls.mostRecent().returnValue);
+            });
         });
     });
 });
