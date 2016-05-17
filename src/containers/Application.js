6543e80b7a6ad082c6902d1c1b1773bb470124b8
Joshua Minzner <josh@minzner.org>
2016-05-17 13:13:41 -0400

    Render the <AlertManager>.

--- src/containers/Application.js
+++ src/containers/Application.js
@@ -3,11 +3,13 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import NotificationCenter from './NotificationCenter';
+import AlertManager from './AlertManager';
 
 export class Application extends Component {
     render() {
         return (<div>
             <NotificationCenter />
+            <AlertManager />
             {this.props.children}
         </div>);
     }
