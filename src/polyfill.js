4ba76944a9b062b1368713bbf9d990e633f1b48a
Joshua Minzner <josh@minzner.org>
2016-06-17 12:12:09 -0400

    Patch react-chartist to support changing responsive options.

--- src/polyfill.js
+++ src/polyfill.js
@@ -1,2 +1,15 @@
 import 'core-js/fn/promise';
 import 'isomorphic-fetch';
+import ChartistGraph from 'react-chartist';
+
+{
+    const updateChart = ChartistGraph.prototype.updateChart;
+
+    ChartistGraph.prototype.updateChart = function updateChartPatch(config) {
+        if (this.chartist) {
+            this.chartist.responsiveOptions = config.responsiveOptions || [];
+        }
+
+        return updateChart.call(this, config);
+    };
+}
