8d30fee4f8e09aa44337c2499d3e278d17333cce
Joshua Minzner <josh@minzner.org>
2016-06-20 17:17:32 -0400

    Rewrite CampaignDetailChart to use chart.js.

--- src/components/CampaignDetailChart.js
+++ src/components/CampaignDetailChart.js
@@ -1,112 +1,167 @@
-import React, { PropTypes } from 'react';
-import ChartistGraph from 'react-chartist';
+import React, { PropTypes, Component } from 'react';
+import Chart from 'chart.js';
 import moment from 'moment';
-import numeral from 'numeral';
+import { isEqual } from 'lodash';
 
-function formatter({ template, hideLast = false, showEvery = 1 }) {
-    return function format(label, index, labels) {
-        if (
-            (hideLast && index === (labels.length - 1)) ||
-            (index % showEvery !== 0)
-        ) { return ''; }
-
-        return label.format(template);
-    };
+function getSetData(items, prop) {
+    return items.map(item => item[prop] || null);
 }
 
-function ViewsChart({
-    items,
-}) {
+function getLabels(items) {
     const is30Day = items.length > 7;
 
-    return (<ChartistGraph
-        type="Line"
-        data={{
-            series: [items.map(({ users }) => users || null)],
-            labels: items.map(({ date }) => moment(date)),
-        }}
-        options={{
-            axisX: {
-                showGrid: false,
-                labelInterpolationFnc: is30Day ?
-                    formatter({ template: 'M/D', showEvery: 2 }) :
-                    formatter({ template: 'dddd', hideLast: true }),
+    return is30Day ?
+        items.map(({ date }) => moment(date).format('M/D')) :
+        items.map(({ date }) => moment(date).format('dddd M/D'));
+}
+
+function isEmpty(items) {
+    return !items || !items.some(({ users, clicks }) => users || clicks);
+}
+
+export default class CampaignDetailChart extends Component {
+    constructor(...args) {
+        super(...args);
+
+        this.chart = null;
+    }
+
+    componentDidMount() {
+        this.createChart(this.props.items);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (isEqual(nextProps.items, this.props.items)) { return undefined; }
+
+        const {
+            items,
+        } = nextProps;
+
+        if (isEmpty(items)) {
+            return this.cleanup();
+        }
+
+        if (!this.chart) {
+            return undefined;
+        }
+
+        const { data } = this.chart;
+        const [userSet, clickSet] = data.datasets;
+
+        data.labels = getLabels(items);
+        userSet.data = getSetData(items, 'users');
+        clickSet.data = getSetData(items, 'clicks');
+
+        return this.chart.update();
+    }
+
+    componentDidUpdate() {
+        this.createChart(this.props.items);
+    }
+
+    componentWillUnmount() {
+        this.cleanup();
+    }
+
+    cleanup() {
+        this.chart.destroy();
+        this.chart = null;
+    }
+
+    createChart(items) {
+        const {
+            canvas,
+        } = this.refs;
+
+        if (!canvas || this.chart) { return; }
+
+        this.chart = new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: getLabels(items),
+                datasets: [
+                    {
+                        label: 'Unique Views',
+                        data: getSetData(items, 'users'),
+                        fill: false,
+                        backgroundColor: 'rgba(38, 173, 228,1)',
+                        borderColor: 'rgba(38, 173, 228,1)',
+                        pointBorderColor: 'rgba(38, 173, 228,1)',
+                        pointBackgroundColor: 'rgba(38, 173, 228,1)',
+                        lineTension: 0,
+                        yAxisID: 'users',
+                    },
+                    {
+                        label: 'Clicks',
+                        data: getSetData(items, 'clicks'),
+                        fill: false,
+                        backgroundColor: 'rgba(122, 179, 23,1)',
+                        borderColor: 'rgba(122, 179, 23,1)',
+                        pointBorderColor: 'rgba(122, 179, 23,1)',
+                        pointBackgroundColor: 'rgba(122, 179, 23,1)',
+                        lineTension: 0,
+                        yAxisID: 'clicks',
+                    },
+                ],
             },
-            axisY: {
-                labelInterpolationFnc: label => numeral(label).format('0,0'),
-            },
-            lineSmooth: false,
-            showArea: true,
-            showPoint: false,
-            fullWidth: true,
-        }}
-        responsiveOptions={[
-            ['screen and (max-width: 700px)', {
-                axisX: {
-                    labelInterpolationFnc: is30Day ?
-                        formatter({ template: 'M/D', showEvery: 5 }) :
-                        formatter({ template: 'dd', hideLast: true }),
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'bottom',
                 },
-            }],
-        ]}
-    />);
-}
-ViewsChart.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
-        users: PropTypes.number.isRequired,
-        date: PropTypes.string.isRequired,
-    }).isRequired).isRequired,
-};
+                hoverMode: 'label',
+                borderWidth: 2,
 
-function ClicksChart({
-    items,
-}) {
-    return (<ChartistGraph
-        type="Line"
-        data={{
-            series: [items.map(({ clicks }) => clicks || null)],
-        }}
-        options={{
-            axisX: {
-                showGrid: false,
-                showLabel: false,
+                stacked: true,
+                scales: {
+                    xAxes: [
+                        {
+                            display: true,
+                            gridLines: {
+                                offsetGridLines: false,
+                            },
+                        },
+                    ],
+                    yAxes: [
+                        {
+                            type: 'linear',
+                            display: true,
+                            position: 'left',
+                            id: 'users',
+                        },
+                        {
+                            type: 'linear',
+                            display: true,
+                            position: 'right',
+                            id: 'clicks',
+
+                            gridLines: {
+                                drawOnChartArea: false,
+                            },
+                        },
+                    ],
+                },
             },
-            axisY: {
-                showGrid: false,
-                showLabel: true,
-                position: 'end',
-            },
-            lineSmooth: false,
-            showArea: true,
-            showPoint: false,
-            fullWidth: true,
-        }}
-    />);
+        });
+    }
+
+    render() {
+        const {
+            items,
+        } = this.props;
+        const empty = isEmpty(items);
+
+        return (<div className="campaign-stats-chart col-md-12 col-sm-12">
+            {empty && (<div className="empty-chart">
+                <div className="no-data-message">
+                    We don’t have enough data on your ad to show you these stats yet. Your ad is
+                    working hard. Check back soon to see how it’s doing.
+                </div>
+            </div>)}
+            {!empty && <canvas ref="canvas" />}
+        </div>);
+    }
 }
-ClicksChart.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.shape({
-        clicks: PropTypes.number.isRequired,
-        date: PropTypes.string.isRequired,
-    }).isRequired).isRequired,
-};
-
-export default function CampaignDetailChart({
-    items,
-}) {
-    const isEmpty = (!items || !items.some(({ users, clicks }) => users || clicks));
-
-    return (<div className="campaign-stats-chart col-md-12 col-sm-12">
-        {isEmpty && (<div className="empty-chart">
-            <div className="no-data-message">
-                We don’t have enough data on your ad to show you these stats yet. Your ad is
-                working hard. Check back soon to see how it’s doing.
-            </div>
-        </div>)}
-        {!isEmpty && <ViewsChart items={items} />}
-        {!isEmpty && <ClicksChart items={items} />}
-    </div>);
-}
-
 CampaignDetailChart.propTypes = {
     items: PropTypes.arrayOf(PropTypes.shape({
         users: PropTypes.number.isRequired,
