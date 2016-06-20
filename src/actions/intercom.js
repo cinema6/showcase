bfc8fa0b86d25ab4535b17f9daf621e0e65c9ad1
Scott Munson <sqmunson@gmail.com>
2016-06-20 15:49:37 -0400

    add action/thunk for tracking successful login

---
+++ src/actions/intercom.js
@@ -0,0 +1,14 @@
+import { createThunk } from '../middleware/fsa_thunk';
+import intercom from '../utils/intercom';
+
+export const intercomTrackLogin = createThunk(user => (
+    function trackLogin() {
+        return intercom.track('boot', {
+            app_id: intercom.id,
+            name: user.firstName + ' ' + user.lastName,
+            email: user.email,
+            created_at: user.created,
+            rc_app: 'showcase apps'
+        });
+    }
+));
