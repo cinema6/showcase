8695b07f68754ba60441d4b8944a087826c537d4
Joshua Minzner <josh@minzner.org>
2016-06-23 17:46:46 -0400

    Make sure that all signup form fields are filled in.

--- test/spec/sign_up.form.ut.js
+++ test/spec/sign_up.form.ut.js
@@ -10,6 +10,16 @@ import { assign } from 'lodash';
 import  defer from 'promise-defer';
 import signUpReducer from '../../src/reducers/form/sign_up';
 
+function type(input, text = '') {
+    input.simulate('focus');
+    input.simulate('change', {
+        target: {
+            value: text
+        }
+    });
+    input.simulate('blur');
+}
+
 describe('SignUp', function() {
     describe('when rendered', function() {
         let state, formReducer, store;
@@ -67,34 +77,123 @@ describe('SignUp', function() {
 
         describe('when the form is submitted', function() {
             let submitDeferred;
+            let firstName, lastName, email, password;
 
             beforeEach(function() {
+                firstName = component.find('input').at(0);
+                lastName = component.find('input').at(1);
+                email = component.find('input').at(2);
+                password = component.find('input').at(3);
+
                 props.onSubmit.and.returnValue((submitDeferred = defer()).promise);
-
-                component.find('form').simulate('submit');
             });
 
-            it('should call onSubmit() with the values', function() {
-                expect(props.onSubmit).toHaveBeenCalledWith({
-                    firstName: undefined,
-                    lastName: undefined,
-                    email: undefined,
-                    password: undefined
+            describe('without the required values', function() {
+                beforeEach(function() {
+                    [firstName, lastName, email, password].forEach(input => input.simulate('blur'));
+
+                    component.find('form').simulate('submit');
+                });
+
+                it('should show all of the errors', function() {
+                    [firstName, lastName, email, password].forEach(input => {
+                        const group = input.parent();
+                        const help = group.find('.help-block');
+
+                        expect(group.hasClass('has-error')).toBe(true, `${input.prop('name')} has no "has-error" class.`);
+                        expect(help.length).toBe(1, `${input.prop('name')} has no .help-block.`);
+                    });
                 });
             });
 
-            describe('if the promise is returns is rejected', function() {
-                let reason;
+            describe('with all of the required values', function() {
+                beforeEach(function() {
+                    type(firstName, 'Jason');
+                    type(lastName, 'Glickman');
+                    type(email, 'jason@ceospace.com');
+                    type(password, 'banana');
 
-                beforeEach(function(done) {
-                    reason = new Error('It went wrong!');
-
-                    submitDeferred.reject(reason);
-                    setTimeout(done);
+                    component.find('form').simulate('submit');
                 });
 
-                it('should return an error with an _error property', function() {
-                    expect(component.props().error).toBe(reason);
+                it('should call onSubmit() with the values', function() {
+                    expect(props.onSubmit).toHaveBeenCalledWith({
+                        firstName: 'Jason',
+                        lastName: 'Glickman',
+                        email: 'jason@ceospace.com',
+                        password: 'banana'
+                    });
+                });
+
+                describe('if the promise is returns is rejected', function() {
+                    let reason;
+
+                    beforeEach(function(done) {
+                        reason = new Error('It went wrong!');
+
+                        submitDeferred.reject(reason);
+                        setTimeout(done);
+                    });
+
+                    it('should return an error with an _error property', function() {
+                        expect(component.props().error).toBe(reason);
+                    });
+                });
+            });
+        });
+
+        ['firstName', 'lastName', 'email', 'password'].forEach((field, index) => {
+            describe(`the ${field} field`, function() {
+                beforeEach(function() {
+                    this.group = component.find('.form-group').at(index);
+                    this.input = this.group.find('input.form-control');
+                });
+
+                it('should not have the has-error class', function() {
+                    expect(this.group.hasClass('has-error')).toBe(false, `${field} has "has-error" class.`);
+                });
+
+                it('should not render a .help-block', function() {
+                    expect(this.group.find('.help-block').length).toBe(0, 'A .help-block is rendered.');
+                });
+
+                describe('if the user types in the field', function() {
+                    beforeEach(function() {
+                        this.input.simulate('focus');
+                        this.input.simulate('change', {
+                            target: {
+                                value: 'f'
+                            }
+                        });
+                    });
+
+                    it('should not have the has-error class', function() {
+                        expect(this.group.hasClass('has-error')).toBe(false, `${field} has "has-error" class.`);
+                    });
+
+                    it('should not render a .help-block', function() {
+                        expect(this.group.find('.help-block').length).toBe(0, 'A .help-block is rendered.');
+                    });
+
+                    describe('and deletes their input', function() {
+                        beforeEach(function() {
+                            this.input.simulate('change', {
+                                target: {
+                                    value: ''
+                                }
+                            });
+                            this.input.simulate('blur');
+                            this.input.simulate('focus');
+                        });
+
+                        it('should have the has-error class', function() {
+                            expect(this.group.hasClass('has-error')).toBe(true, `${field} has no "has-error" class.`);
+                        });
+
+                        it('should render a .help-block', function() {
+                            expect(this.group.find('.help-block').length).toBe(1, 'A .help-block is not rendered.');
+                        });
+                    });
                 });
             });
         });
