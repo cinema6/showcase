3009f47f857a76d3dc44cfe58e767a8445c5010d
Rylan Gupta <rylander77@gmail.com>
2016-07-14 15:29:53 -0400

    Added globals.js as globals directory, also allows before, beforeEach, adter, and afterEach methods to be added later. Got rid of rbowser blocks

---
+++ test/e2e/helpers/globals.js
@@ -0,0 +1,8 @@
+module.exports = {
+    email: 'email@cinema6.com',
+    password: 'bananas4bananas',
+    firstName: 'Johnny',
+    lastName: 'Testmonkey',
+    company: 'Company',
+    app: 'Pokémon GO'
+};
