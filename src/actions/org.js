e4c727a209823cd82fde1c1909844806cbc1fa9d
Joshua Minzner <josh@minzner.org>
2016-07-28 11:54:13 -0400

    Add actions for changing the user's payment plan.

--- src/actions/org.js
+++ src/actions/org.js
@@ -1,6 +1,30 @@
 import { createDbActions } from '../utils/db';
+import { callAPI } from './api';
+
+function prefix(type) {
+    return `ORG/${type}`;
+}
 
 export default createDbActions({
     type: 'org',
     endpoint: '/api/account/orgs',
+});
+
+export const CHANGE_PAYMENT_PLAN_START = prefix('CHANGE_PAYMENT_PLAN_START');
+export const CHANGE_PAYMENT_PLAN_SUCCESS = prefix('CHANGE_PAYMENT_PLAN_SUCCESS');
+export const CHANGE_PAYMENT_PLAN_FAILURE = prefix('CHANGE_PAYMENT_PLAN_FAILURE');
+export const changePaymentPlan = ({
+    orgId,
+    paymentPlanId,
+}) => callAPI({
+    method: 'POST',
+    endpoint: `/api/account/orgs/${orgId}/payment-plan`,
+    types: [
+        CHANGE_PAYMENT_PLAN_START,
+        CHANGE_PAYMENT_PLAN_SUCCESS,
+        CHANGE_PAYMENT_PLAN_FAILURE,
+    ],
+    body: {
+        id: paymentPlanId,
+    },
 });
